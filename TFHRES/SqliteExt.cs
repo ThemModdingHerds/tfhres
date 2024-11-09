0cd5237ea11ae31a06306714936f6280884ea969
Night The Fox <kilo.night.prower@gmail.com>
2024-11-10 00:19:56 +0100

    THIS IS THE LAST COMMIT I HOPE SO + also cool picture

--- TFHRES/SqliteExt.cs
+++ TFHRES/SqliteExt.cs
@@ -4,13 +4,7 @@ using Microsoft.Data.Sqlite;
 namespace ThemModdingHerds.TFHResource;
 public static class SqliteExt
 {
-    public static T GetValue<T>(this SqliteDataReader reader,string name,T defaultValue)
-    {
-        object value = reader.GetValue(name);
-        if(value is DBNull)
-            return defaultValue;
-        return (T)value;
-    }
+    public static T GetValue<T>(this SqliteDataReader reader,string name,T defaultValue) => reader.GetFieldValue<T>(name) ?? defaultValue;
     public static long GetHiberliteId(this SqliteDataReader reader) => GetInteger(reader,"hiberlite_id");
     public static string GetText(this SqliteDataReader reader,string name) => GetValue(reader,name,string.Empty);
     public static long GetInteger(this SqliteDataReader reader,string name) => GetValue(reader,name,-1L);
